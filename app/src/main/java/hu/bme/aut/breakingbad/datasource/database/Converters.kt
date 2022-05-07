5075f1e179e90850cce99be2ccd43e5a80d60c0e
Gabor1998 <stukszagabor@freemail.hu>
2022-05-07 18:25:02 +0200

    Add database tests

--- app/src/main/java/hu/bme/aut/breakingbad/datasource/database/Converters.kt
+++ app/src/main/java/hu/bme/aut/breakingbad/datasource/database/Converters.kt
@@ -2,6 +2,7 @@ package hu.bme.aut.breakingbad.datasource.database
 
 import androidx.room.TypeConverter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class Converters {
@@ -9,18 +10,16 @@ class Converters {
     private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     @TypeConverter
-    fun jsonToList(json: String): List<String> = moshi.fromJson<List<String>>(json).orEmpty()
+    fun jsonToList(json: String): List<String> = moshi.adapter<List<String>>(Types.newParameterizedType(List::class.java, String::class.javaObjectType)).fromJson(json).orEmpty()
 
     @TypeConverter
     fun listToJson(list: List<String>): String = moshi.toJson(list)
 
     @TypeConverter
-    fun jsonToListInt(json: String): List<Int> = moshi.fromJson<List<Int>>(json).orEmpty()
+    fun jsonToListInt(json: String): List<Int> = moshi.adapter<List<Int>>(Types.newParameterizedType(List::class.java, Int::class.javaObjectType)).fromJson(json).orEmpty()
 
     @TypeConverter
     fun intListToJson(list: List<Int>): String = moshi.toJson(list)
 }
-
-inline fun <reified T> Moshi.fromJson(json: String) = this.adapter(T::class.java).fromJson(json)
 
 inline fun <reified T> Moshi.toJson(value: T): String = this.adapter(T::class.java).toJson(value)
